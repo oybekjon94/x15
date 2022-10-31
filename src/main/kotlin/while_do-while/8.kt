892076fcbf3ed5ab8083a397fa0067ee8ed9f846
oybekjon94 <x.oybek94@gmail.com>
2022-10-31 16:53:43 +0900

    [feat::] Add 8 while

---
+++ src/main/kotlin/while_do-while/8.kt
@@ -0,0 +1,19 @@
+package `while_do-while`
+
+fun main(){
+    var i = 1
+    while (1 < 9){
+        if (i == 5){
+            break
+        }
+        println(i)
+        i += 1
+    }
+}
+/*
+output
+1
+2
+3
+4
+ */
