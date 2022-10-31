712fd2eff72892660220d33602b2f8add5f6582d
oybekjon94 <x.oybek94@gmail.com>
2022-10-31 16:09:53 +0900

    [feat::] Add 6 do-while

---
+++ src/main/kotlin/while_do-while/6.kt
@@ -0,0 +1,10 @@
+package `while_do-while`
+
+fun main(){
+    val name = arrayOf("oybek","sherbek","nurbek","otabek")
+    var index = 0
+    while (index < name.size){
+        println(name[index] + " kholikov")
+        index++
+    }
+}
