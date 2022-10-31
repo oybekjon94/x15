ded2dafda2c70087a75a37a1151cc82da15bb6b5
oybekjon94 <x.oybek94@gmail.com>
2022-10-31 17:48:07 +0900

    [feat::] Add 1 looping/ranges

---
+++ src/main/kotlin/loopingAndRanges/1.kt
@@ -0,0 +1,10 @@
+package loopingAndRanges
+
+fun main(){
+    //The for keyword executes a block of code for each value in a sequence.
+    for (i in 1..10){
+        print(i)
+    }
+}
+//output
+//12345678910
