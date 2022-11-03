2b43718b8a029365e512cfbe30f78fe678c0aba0
oybekjon94 <x.oybek94@gmail.com>
2022-11-03 20:09:44 +0900

    [feat::] Add 9 lists

---
+++ src/main/kotlin/lists/9.kt
@@ -0,0 +1,12 @@
+package lists
+
+fun main(){
+    val guestsPerFamily = listOf(2,3,4,1)
+    var totalGuests = 0
+    var index = 0
+    while (index < guestsPerFamily.size){
+        totalGuests += guestsPerFamily[index]
+        index++
+    }
+    println("Total Guest Count: $totalGuests")
+}
