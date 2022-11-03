0770a05a5f9544e7289bcb2582aaf7cc1eb0223c
oybekjon94 <x.oybek94@gmail.com>
2022-11-03 19:40:05 +0900

    [feat::] Add 1 lists

---
+++ src/main/kotlin/lists/1.kt
@@ -0,0 +1,8 @@
+package lists
+
+//A List is a container, which is an object that holds other objects
+fun main(){
+    val numbers = listOf(1,2,3,4,5,6)
+    println("List: $numbers")
+    println("Size: ${numbers.size}")
+}
