4048d778e9a85e2e55e87813da74774959df0e1e
oybekjon94 <x.oybek94@gmail.com>
2022-11-03 19:32:44 +0900

    [feat::] Add 15 exception

---
+++ src/main/kotlin/exception/15.kt
@@ -0,0 +1,12 @@
+package exception
+
+fun main(){
+    try {
+        var int = 10 / 0
+        println(int)
+    }catch (e: ArithmeticException){
+        println(e)
+    }finally {
+        println("This block always executes")
+    }
+}
