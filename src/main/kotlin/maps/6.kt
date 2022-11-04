ac017cc4f8334132fea0d19eab931b5a2d92b656
oybekjon94 <x.oybek94@gmail.com>
2022-11-04 16:05:12 +0900

    [feat::] Add 6 maps

---
+++ src/main/kotlin/maps/6.kt
@@ -0,0 +1,11 @@
+package maps
+
+fun main(){
+    val menu = mapOf("Shashlik" to 15000,
+    "somsa" to 5000,
+    "osh" to 10000,
+    "shurva" to 6000)
+
+    println(menu.keys)
+    println(menu.values)
+}
