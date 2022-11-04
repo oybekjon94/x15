323de826c73b1dc830650b226102589acb6e2707
oybekjon94 <x.oybek94@gmail.com>
2022-11-04 16:25:58 +0900

    [feat::] Add 7 maps

--- src/main/kotlin/maps/7.kt
+++ src/main/kotlin/maps/7.kt
@@ -1,2 +1,14 @@
 package maps
 
+fun main(){
+    val name = mutableMapOf(1 to "kholikov",2  to "oybek")
+    name.put(3, "sherbek")
+    name.put(4, "nurbek")
+    for (i in name.keys){
+        println(name.get(i))
+    }
+
+    for (i in name){
+        println(name)
+    }
+}
