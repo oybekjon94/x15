cf36bf8840c82a0d3e436973d80c8e10b769da2b
oybekjon94 <x.oybek94@gmail.com>
2022-11-04 16:31:30 +0900

    [feat::] Add 8 maps

---
+++ src/main/kotlin/maps/8.kt
@@ -0,0 +1,10 @@
+package maps
+
+fun main(){
+    val map = mapOf(1 to "one", 2 to "two",
+    3 to "three",4 to "four")
+
+    println("map entries: "+map)
+    println("map keys: "+map.keys)
+    println("map values: "+map.values)
+}
