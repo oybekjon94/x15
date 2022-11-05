28148d55b3aa8e9df132abf855069296d49914d5
oybekjon94 <x.oybek94@gmail.com>
2022-11-06 04:08:36 +0900

    [feat::] Add 4 when

---
+++ src/main/kotlin/when/5.kt
@@ -0,0 +1,22 @@
+package `when`
+
+fun main(){
+    print("enter number of the month: ")
+    var monthOfYear = readLine()!!.toInt()
+    var month = when(monthOfYear){
+        1->"january"
+        2->"February"
+        3->"March"
+        4->"April"
+        5->"May"
+        6->"June"
+        7->"July"
+        8->"August"
+        9->"September"
+        10->"October"
+        11->"November"
+        12->"December"
+        else->"not a month of year"
+    }
+    print(month)
+}
