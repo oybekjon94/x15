0746d5b163c7c3a67b466c91b9ce90001e000b29
oybekjon94 <x.oybek94@gmail.com>
2022-11-06 09:09:20 +0900

    [feat::] Add 4 enum

--- src/main/kotlin/enum/4.kt
+++ src/main/kotlin/enum/4.kt
@@ -1,2 +1,15 @@
 package enum
 
+import java.lang.instrument.ClassDefinition
+
+fun main(){
+    val p1 = Person("Ali",20,Gender.MAN)
+    val p2 = Person("Alice",20,Gender.FEMALE)
+    println("${p1.name}, ${p1.age}, ${p1.gender.definition}")
+    println("${p2.name}, ${p2.age}, ${p2.gender.definition}")
+}
+class Person(val name: String, val age: Int, val gender: Gender)
+enum class Gender(val definition: String){
+    MAN("erkak"),
+    FEMALE("ayol")
+}
